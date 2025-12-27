3b26556b655b15d2e2c1a465d6db7646ee576e6b
Mohanakri <mohanakrishna109@gmail.com>
2025-12-27 14:35:40 +0530

    add

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,10 @@
+import 'dotenv/config';
 import mongoose from 'mongoose';
-import app from './app';
+import app from './app.js';
 
-mongoose.connect(process.env.MONGO_URI!).then(()=>{
-  app.listen(4000, ()=> console.log('Server running'));
-});
+mongoose.connect(process.env.MONGO_URI!)
+  .then(() => app.listen(4000, () => console.log("Backend running")))
+  .catch(err => {
+    console.error("DB Failed", err);
+    process.exit(1);
+  });
